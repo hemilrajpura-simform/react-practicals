e85484658a1108f95d0d0590e8f5d1dca6c217b3
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-03-03 13:57:49 +0530

    final config files for submission

--- Practical-2/webpack.dev.js
+++ Practical-2/webpack.dev.js
@@ -1,3 +1,23 @@
-module.exports = {
-    mode: "development",
-};
+const {merge} = require('webpack-merge');
+const baseConfig = require('./webpack.config');
+
+const path = require("path");
+
+module.exports = merge(baseConfig, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, "public/"),
+      port: 3000,
+      publicPath: "http://localhost:3000/",
+      hotOnly: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+      ]
+    }
+  })
