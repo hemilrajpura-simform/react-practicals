4a46eebf815355d13db86b97965d1b853dae0e2a
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-03-04 16:59:40 +0530

    Initial structure for the practical 6 - API Integration

---
+++ Practical-05/user-list/src/Data.js
@@ -0,0 +1,73 @@
+// PRACTICAL-5
+
+
+// export const users =
+//     [
+//         {
+//             userId: 1,
+//             image: 'https://www.kindpng.com/picc/m/130-1300298_female-user-icon-svg-clip-arts-female-user.png',
+//             username: 'Bhumika Soomani',
+//             email: 'bhumika.s@yahoo.com',
+//             status: true,
+//             access: 'Owner',
+//             clicksReviewed: 10450,
+//             MonthlyClicks:8024,
+//             planUses: 95
+//         },
+//         {
+//             userId: 2,
+//             image: 'https://www.kindpng.com/picc/m/630-6306130_avatar-avatar-male-user-icon-hd-png-download.png',
+//             username: 'Abhishek Chandarana',
+//             email: 'abhishek.c@yahoo.com',
+//             status: false,
+//             access: 'Manager',
+//             clicksReviewed: 9150,
+//             MonthlyClicks:6251,
+//             planUses: 80
+            
+//         },
+//         {
+//             userId: 3,
+//             image: 'https://mpng.subpng.com/20180403/tkw/kisspng-avatar-computer-icons-user-profile-business-user-avatar-5ac3a1f7d96614.9721182215227704238905.jpg',
+//             username: 'Hemil Rajpura',
+//             email: 'hemil.r@yahoo.com',
+//             status: false,
+//             access: 'Read',
+//             clicksReviewed: 2450,
+//             MonthlyClicks:5024,
+//             planUses: 86
+//         },
+//         {
+//             userId: 4,
+//             image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyXOkihz3b3jxBWZxp5Chr5p9XAVvTOdhivtL1EBmJF7ack1LRjg_P20jbfd8B6LmFOVQ&usqp=CAU',
+//             username: 'Harsh Patel',
+//             email: 'harsh.p@yahoo.com',
+//             status: false,
+//             access: 'Read',
+//             clicksReviewed: 2400,
+//             MonthlyClicks:5000,
+//             planUses: 71
+//         },
+//         {
+//             userId: 5,
+//             image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQarfJZaR_AwPKKU8BEsVCGs-Bt7XcE7Y8jL67PPX3gcPMOXpl76C0C0nsabRz6wm9pEhU&usqp=CAU',
+//             username: 'Sagar Chhatbar',
+//             email: 'sagar.c@yahoo.com',
+//             status: false,
+//             access: 'Read',
+//             clicksReviewed: 2650,
+//             MonthlyClicks:5030,
+//             planUses: 81
+//         },
+//         {
+//             userId: 6,
+//             image: 'https://www.kindpng.com/picc/m/163-1636340_user-avatar-icon-avatar-transparent-user-icon-png.png',
+//             username: 'Anjali Shah',
+//             email: 'anjali.s@yahoo.com',
+//             status: false,
+//             access: 'Read',
+//             clicksReviewed: 2471,
+//             MonthlyClicks:5200,
+//             planUses: 49
+//         }
+//     ];
--- Practical-05/user-list/src/components/UserList.js
+++ Practical-05/user-list/src/components/UserList.js
@@ -1,13 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import UserItem from './UserItem';
 import Header from './Header';
-import { users } from '../data';
+// import { Users } from '../Data'; 
+import PageNotFound from './PageNotFound';
 import './UserList.css';
 import Popup from './Popup';
 
 const UserList = (props) => {
     const [showPopup, setShowPopup] = useState(false);
     const [selectedUser, setSelectedUser] = useState();
+
+    const [pageFound, setPageFound] = useState(false);
+    const [pageLoading, setPageLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const paginationHandler = (number) => {
+        if (number === "plus") {
+            setCurrentPage(() => { return currentPage + 1 });
+            return;
+        }
+        else if (number === "minus") {
+            console.log("cur page", currentPage);
+            setCurrentPage(() => { return currentPage - 1 });
+            console.log("cur page", currentPage);
+            return;
+        }
+        else if (number === "reload") {
+            setCurrentPage(1);
+            return;
+        }
+        setCurrentPage(number);
+    };
+
+
+
+    const [users, setUsers] = useState([]);
+
+
+    const loadingHandler = () => {
+        if (pageLoading) {
+            console.log('loading');
+        }
+        else {
+            console.log('Not loading');
+        }
+    };
+    loadingHandler();
+
+    useEffect(() => {
+        const f = async () => {
+            setPageLoading(true);
+            const res = await fetch("https://reqres.in/api/users?page=" + currentPage.toString());
+            const json = await res.json();
+            setUsers(json.data);
+            setPageLoading(false);
+        };
+
+        const pageFoundHandler = () => {
+            if (users.length !== 0) {
+                setPageFound(true);
+            }
+            else {
+                setPageFound(false);
+            }
+
+        };
+        f();
+        pageFoundHandler();
+
+
+    }, [currentPage, users.length]);
+
 
     const closePopup = () => {
         setShowPopup(false);
@@ -18,31 +81,48 @@ const UserList = (props) => {
         console.log('item obj', selectedUser);
     };
 
+    const paginationStyle = {
+        outline: "2px solid black",
+    };
+    return (!pageFound ? <PageNotFound paginationHandler={paginationHandler} /> : (
 
-    return (
         <div className='main-container' onMouseLeave={closePopup}>
             <div className='user-list-main'>
                 <Header />
                 {
                     users.map(item => (
-                        <div key={item.userId} >
+                        <div key={item.id} >
                             <UserItem
                                 showPopupHandler={showPopupHandler}
-                                username={item.username}
-                                image={item.image}
+                                username={item.first_name + " " + item.last_name}
+                                image={item.avatar}
                                 email={item.email}
                                 status={item.status}
-                                access={item.access}
-                                userId={item.userId}
-                                MonthlyClicks={item.MonthlyClicks}
-                                clicksReviewed={item.clicksReviewed}
-                                id={item.userId}
+                                access={item.access="Read"}
+                                userId={item.id}
+                                MonthlyClicks={item.MonthlyClicks=8021}
+                                clicksReviewed={item.clicksReviewed=7541}
+                                id={item.id}
                                 closePopup={closePopup}
                             />
                         </div>
                     ))
                 }
 
+
+                <div className='pagination'>
+                    <button
+                        onClick={() => paginationHandler("minus")}> {"<"} </button>
+                    <button
+                        style={currentPage === 1 ? paginationStyle : null}
+                        onClick={() => paginationHandler(1)}>1</button>
+
+                    <button
+                        style={currentPage === 2 ? paginationStyle : null}
+                        onClick={() => paginationHandler(2)}>2</button>
+                    <button
+                        onClick={() => paginationHandler("plus")}> {">"} </button>
+                </div>
             </div>
 
             {showPopup && (
@@ -58,6 +138,7 @@ const UserList = (props) => {
             )}
         </div>
 
+    )
     );
 };
 
